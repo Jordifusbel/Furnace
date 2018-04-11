ddba61b2c79de394c7b45d08b545b360828539cb
Jordi <jordifusbel@hotmail.com>
2018-04-11 16:24:17 +0200

    Corregido lugar de temperature

--- src/main/java/org/mvpigs/ambientTemperature/AmbientTemperature.java
+++ src/main/java/org/mvpigs/ambientTemperature/AmbientTemperature.java
@@ -1,4 +1,13 @@
 package org.mvpigs.ambientTemperature;
 
 public class AmbientTemperature {
+    private double ambientTemperature = 15;
+
+    public double getAmbientTemperature() {
+        return ambientTemperature;
+    }
+
+    public void setAmbientTemperature(double ambientTemperature) {
+        this.ambientTemperature = ambientTemperature;
+    }
 }
--- src/main/java/org/mvpigs/thermometer/ThermometerImpl.java
+++ src/main/java/org/mvpigs/thermometer/ThermometerImpl.java
@@ -3,7 +3,7 @@ package org.mvpigs.thermometer;
 import org.mvpigs.ambientTemperature.AmbientTemperature;
 
 public class ThermometerImpl implements Thermometer {
-    private double ambientTemp = 15;
+
 
     public Double read(AmbientTemperature temp){
 
