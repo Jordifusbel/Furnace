34b0cf7916be09265df55535838f28a2c795f1b1
Jordi <jordifusbel@hotmail.com>
2018-04-11 16:51:08 +0200

    Creado caso test

--- src/test/java/org/mvpigs/regulatorTest.java
+++ src/test/java/org/mvpigs/regulatorTest.java
@@ -3,18 +3,22 @@ package org.mvpigs;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.mvpigs.ambientTemperature.AmbientTemperature;
+import org.mvpigs.heater.HeaterImpl;
+import org.mvpigs.thermometer.ThermometerImpl;
 
-/**
- * Unit test for simple Regulator.
- */
+
 public class regulatorTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void furnaceTest()
     {
-        assertTrue( true );
+        Regulator regulator = new Regulator();
+        ThermometerImpl t = new ThermometerImpl();
+        HeaterImpl h = new HeaterImpl();
+        AmbientTemperature temp = new AmbientTemperature();
+
+        regulator.regulate(t,h,10.0,50.0, temp);
     }
 }
